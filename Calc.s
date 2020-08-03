2e90ca98b15cca2f80979c65f5c85f36b2fe0e3e
Victor Abedi <v.nyangweso@carepay.co.ke>
2020-08-03 20:46:08 +0300

    Trail asm covertion

---
+++ Calc.s
@@ -0,0 +1,251 @@
+	.file	"Calc.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+.LC0:
+	.string	"Enter first number \n"
+.LC1:
+	.string	"Enter second number \n"
+.LC2:
+	.string	"Answer = "
+	.section	.text._ZN10Calculator3sumEv,"axG",@progbits,_ZN10Calculator3sumEv,comdat
+	.align 2
+	.weak	_ZN10Calculator3sumEv
+	.type	_ZN10Calculator3sumEv, @function
+_ZN10Calculator3sumEv:
+.LFB1522:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	leaq	.LC0(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC1(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	-8(%rbp), %rax
+	addq	$4, %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	movq	-8(%rbp), %rax
+	movl	(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	4(%rax), %eax
+	addl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 8(%rax)
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	cltq
+	leaq	.LC2(%rip), %rdx
+	addq	%rdx, %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1522:
+	.size	_ZN10Calculator3sumEv, .-_ZN10Calculator3sumEv
+	.section	.text._ZN10Calculator3subEv,"axG",@progbits,_ZN10Calculator3subEv,comdat
+	.align 2
+	.weak	_ZN10Calculator3subEv
+	.type	_ZN10Calculator3subEv, @function
+_ZN10Calculator3subEv:
+.LFB1523:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	leaq	.LC0(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	leaq	.LC1(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	-8(%rbp), %rax
+	addq	$4, %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	movq	-8(%rbp), %rax
+	movl	(%rax), %edx
+	movq	-8(%rbp), %rax
+	movl	4(%rax), %eax
+	subl	%eax, %edx
+	movq	-8(%rbp), %rax
+	movl	%edx, 8(%rax)
+	movq	-8(%rbp), %rax
+	movl	8(%rax), %eax
+	cltq
+	leaq	.LC2(%rip), %rdx
+	addq	%rdx, %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1523:
+	.size	_ZN10Calculator3subEv, .-_ZN10Calculator3subEv
+	.section	.rodata
+	.align 8
+.LC3:
+	.string	"Basic Calculator \n 1. Addition \n 2. Subtraction"
+.LC4:
+	.string	"Invalid Entry \n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1524:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC3(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	leaq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt3cin(%rip), %rdi
+	call	_ZNSirsERi@PLT
+	movl	-24(%rbp), %eax
+	cmpl	$1, %eax
+	je	.L4
+	cmpl	$2, %eax
+	je	.L5
+	jmp	.L6
+.L4:
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZN10Calculator3sumEv
+.L5:
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rdi
+	call	_ZN10Calculator3subEv
+.L6:
+	leaq	.LC4(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L8
+	call	__stack_chk_fail@PLT
+.L8:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1524:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2008:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L11
+	cmpl	$65535, -8(%rbp)
+	jne	.L11
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L11:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2008:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB2009:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2009:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 9.3.0-10ubuntu2) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
